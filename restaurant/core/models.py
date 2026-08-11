0fa7da493b39397cedad07511c3e65abd3574377
Sujan Lama <sujanlama2323@gmail.com>
2026-08-11 13:55:06 +0545

    add testemonial , dynamic menu

--- restaurant/core/models.py
+++ restaurant/core/models.py
@@ -9,16 +9,23 @@ class Message(models.Model):
 
 class Category(models.Model):
     title = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='categoryImages',null=True)
 
     def __str__(self):
         return self.title
 
 class Momo(models.Model):
     name = models.CharField(max_length=200)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE,related_name='items')
     desc = models.TextField()
     price = models.DecimalField(max_digits=8, decimal_places=2)
     image = models.ImageField(upload_to='momoImage')
     is_available = models.BooleanField(default=True)
     created_at = models.DateField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
+
+class Review(models.Model):
+    name = models.CharField(max_length=200)
+    message = models.TextField()
+    order = models.CharField(max_length=200)
+    rating =models.PositiveSmallIntegerField()
--- restaurant/core/views.py
+++ restaurant/core/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render,redirect
-from .models import Message,Category,Momo
+from .models import Message,Category,Momo,Review
 from django.contrib import messages
+import qrcode
 # Create your views here.
 def index(request):
     category = Category.objects.all()
     cateid = request.GET.get('category')
-    if cateid:
+    if cateid == 'all':
+        momo = Momo.objects.filter(is_available=True)
+    elif cateid:
         momo = Momo.objects.filter(is_available=True,category=cateid)
     else:
         momo = Momo.objects.filter(is_available=True)
@@ -32,14 +35,36 @@ def contact(request):
     return render(request,'core/contact.html')
 
 def menu(request):
-    return render(request,'core/menu.html')
+    category = Category.objects.all()
+    qr = qrcode.make('http://127.0.0.1:8000/menu/')
+    qr.save("core/static/images/qr.png")
+    context ={
+        'category':category
+    }
+
+    return render(request,'core/menu.html',context)
 
 def services(request):
     return render(request,'core/services.html')
 
 
 def testemonial(request):
-    return render(request,'core/testemonial.html')
+    momo = Momo.objects.all()
+    testemonial = Review.objects.all()
+    if request.method == "POST":
+        name = request.POST.get('name')
+        rating = request.POST.get('rating')
+        message = request.POST.get('message')
+        order = request.POST.get('order')
+
+        Review.objects.create(name=name,rating=rating,message=message,order=order)
+
+        return redirect('testemonial')
+    context = {
+        'momo':momo,
+        'testemonial':testemonial
+    }
+    return render(request,'core/testemonial.html',context)
 
 def help(request):
     return render(request,'core/help.html')
